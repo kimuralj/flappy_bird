bb5ad571db2620258d11889458cc07a27f07990b
Leandro Jun Kimura <kimura@Leandros-MacBook-Air.local>
2023-02-21 19:12:18 -0300

    Adding bird movement

--- js/flappy.js
+++ js/flappy.js
@@ -65,10 +65,37 @@ function Barriers(height, width, gap, space, notifyPoint) {
     }
 }
 
-// Testing moving barriers
+function Bird(gameHeight) {
+    let flying = false
+
+    this.element = newElement('img', 'bird')
+    this.element.src = 'imgs/bird.png'
+
+    this.getY = () => parseInt(this.element.style.bottom.split('px')[0])
+    this.setY = y => this.element.style.bottom =`${y}px`
+
+    window.onkeydown = e => flying = true
+    window.onkeyup = e => flying = false
+
+    this.animate = () => {
+        const newY = this.getY() + (flying ? 8 : -5)
+        const maxHeight = gameHeight - this.element.clientHeight
+
+        if (newY <= 0) this.setY(0)
+        else if (newY >= maxHeight) this.setY(maxHeight)
+        else this.setY(newY)
+    }
+
+    this.setY(gameHeight / 2)
+}
+
+// Testing moving barriers and bird
 const barriers = new Barriers(700, 1200, 200, 400)
+const bird = new Bird(700)
 const gameArea = document.querySelector('[flappy]')
+gameArea.appendChild(bird.element)
 barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
 setInterval(() => {
     barriers.animate()
+    bird.animate()
 }, 20)
